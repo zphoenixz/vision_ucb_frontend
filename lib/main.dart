2a121424fa9e755682784b09b6528e3b80dac740
Martín Campuzano <martin.campuzano.flores@gmail.com>
2020-03-23 22:03:22 -0400

    Camera View working and taking pictures completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'views/mainMenu.dart';
 import 'views/camara.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => MyHomePage(),
-        '/camera': (context) => CameraView(),
+        '/camera': (context) => CameraView(camera: firstCamera),
       },
       title: 'NOVISPRO',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-    );
-  }
+    )
+  );
 }
--- lib/views/camara.dart
+++ lib/views/camara.dart
@@ -1,13 +1,46 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraView extends StatefulWidget {
-  const CameraView({Key key}) : super(key: key);
+  final CameraDescription camera;
+
+  const CameraView({Key key, this.camera}) : super(key: key);
 
   @override
   _CameraViewState createState() => _CameraViewState();
 }
 
 class _CameraViewState extends State<CameraView> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     //get status bar height
@@ -53,7 +86,18 @@ class _CameraViewState extends State<CameraView> {
             Flexible(
                 flex: 18,
                 child: Container(
-                  decoration: myBoxDecoration(),
+                  child: FutureBuilder<void>(
+                    future: _initializeControllerFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        // If the Future is complete, display the preview.
+                        return CameraPreview(_controller);
+                      } else {
+                        // Otherwise, display a loading indicator.
+                        return Center(child: CircularProgressIndicator());
+                      }
+                    },
+                  ),
                 )),
             Flexible(
               flex: 3,
@@ -67,10 +111,45 @@ class _CameraViewState extends State<CameraView> {
                             decoration: myBoxDecorationPaper(),
                           ))),
                   Flexible(
-                      flex: 3,
+                    flex: 3,
+                    child: new InkWell(
+                      onTap: () async {
+                        // Take the Picture in a try / catch block. If anything goes wrong,
+                        // catch the error.
+                        try {
+                          // Ensure that the camera is initialized.
+                          await _initializeControllerFuture;
+
+                          // Construct the path where the image should be saved using the
+                          // pattern package.
+                          final path = join(
+                            // Store the picture in the temp directory.
+                            // Find the temp directory using the `path_provider` plugin.
+                            (await getTemporaryDirectory()).path,
+                            '${DateTime.now()}.png',
+                          );
+
+                          // Attempt to take a picture and log where it's been saved.
+                          await _controller.takePicture(path);
+
+                          // If the picture was taken, display it on a new screen.
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  DisplayPictureScreen(imagePath: path),
+                            ),
+                          );
+                        } catch (e) {
+                          // If an error occurs, log the error to the console.
+                          print(e);
+                        }
+                      },
                       child: Container(
                         decoration: myBoxDecorationInfo(),
-                      )),
+                      ),
+                    ),
+                  ),
                   Flexible(
                     flex: 4,
                     child: Hero(
@@ -101,8 +180,8 @@ class _CameraViewState extends State<CameraView> {
   BoxDecoration myBoxDecorationInfo() {
     return BoxDecoration(
       image: DecorationImage(
-        image: new AssetImage('img/info.png'),
-        fit: BoxFit.none,
+        image: new AssetImage('img/camera_circle.png'),
+        fit: BoxFit.scaleDown,
       ),
       color: Color(0xFFC2DFE3),
       border: Border.all(
@@ -165,6 +244,24 @@ class _CameraViewState extends State<CameraView> {
         color: Color(0xFF2050AC), //                   <--- border color
         width: 5.0,
       ),
+    );
+  }
+}
+
+//show Picture
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
     );
   }
 }
